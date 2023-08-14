1e3067db1b6b4294404eec3c7e2eae007ab41e28
Zebib0 <gudetazebib@gmail.com>
2023-08-14 20:30:26 +0000

    task 0 - 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    lst = []
+    for i in range(0, len(my_list)):
+        lst.append(my_list[i])
+    if isx < 0 or idx >= len(my_list):
+        return lst
+    else:
+        lst[idx] = element
+        return lst
