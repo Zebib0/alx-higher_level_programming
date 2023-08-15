9b6206cbb19f8f709b2dffc745227d59a5b44390
Zebib0 <gudetazebib@gmail.com>
2023-08-15 13:36:18 +0000

    fix task 5 and 6

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 def no_c(my_string):
 	c = my_string.count('c')
-	
+        stri = list(stri)
+        while c:
+            stri.remove('c')
+            c -= 1
+        c = stri.count('C')
+        while c:
+            stri.remove('C')
+            c -= 1
+        stri = ''.join(stri)
+        return stri
