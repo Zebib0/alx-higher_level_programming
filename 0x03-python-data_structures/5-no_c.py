1e8ed96a85589211fc433ead9b972c0e7b100d69
Zebib0 <gudetazebib@gmail.com>
2023-08-15 13:41:28 +0000

    error fixed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-	c = my_string.count('c')
-        stri = list(stri)
-        while c:
-            stri.remove('c')
-            c -= 1
-        c = stri.count('C')
-        while c:
-            stri.remove('C')
-            c -= 1
-        stri = ''.join(stri)
-        return stri
+    new_string = ""
+    for c in my_string:
+        if c != "c" and c != "C":
+            new_string += c
+        return new_string
