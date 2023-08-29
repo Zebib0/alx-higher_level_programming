fbbada279765d82b36f234817673b49085c63e60
Zebib0 <gudetazebib@gmail.com>
2023-08-29 19:07:57 +0000

    task 1 fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Creating a new class called Square"""
 
 
 class Square:
+    """class Square that defines a square by: (based on 0-square.py)"""
     def __init__(self, size=0):
-        self.__size = sizei
+        """Initialize Square with size attribute"""
+        self.__size = size
