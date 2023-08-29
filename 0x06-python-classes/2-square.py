97c331a0c5ce1adbca94e154588351d4576b111a
Zebib0 <gudetazebib@gmail.com>
2023-08-29 19:09:29 +0000

    task 3 fixed

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Creating a new class called Square"""
 
 
 class Square:
+    """class Square that defines a square by: (based on 1-square.py)"""
     def __init__(self, size=0):
+        """Initialize Square with size attribute"""
         self.__size = size
 
         if type(size) is not int:
